562597ef4800e9f222234cc615a3bd1ad0d4cca9
ChadCarey <chadcarey7@gmail.com>
2014-09-27 08:41:26 -0600

    set up multithreaded http get requests

---
+++ app/src/main/java/httprequests/Updateable.java
@@ -0,0 +1,9 @@
+package httprequests;
+
+/**
+ * This is an interface for updateable objects
+ * Created by Chad Carey on 9/22/2014.
+ */
+public interface Updateable {
+    public boolean update();
+}
