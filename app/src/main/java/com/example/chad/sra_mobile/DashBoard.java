fb53b40d99122f8cf326ae9ce685626a7701ee3a
Jakob <Jakobhartman@hotmail.com>
2014-10-06 11:01:52 -0600

    Changed dashboard activity to blank activity, changed layout of application flow

---
+++ app/src/main/java/com/example/chad/sra_mobile/DashBoard.java
@@ -0,0 +1,75 @@
+package com.example.chad.sra_mobile;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class DashBoard extends Activity {
+    ListView listView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        listView = (ListView) findViewById(R.id.Interviews);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+
+        String[] values = new String[] {
+                "Bob",
+                "Hudu",
+                "Ben",
+                "Papa Smurf",
+        };
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // ListView Clicked item index
+                int itemP = position;
+                Intent intent = new Intent();
+                // ListView Clicked item value
+                String  itemValue    = (String) listView.getItemAtPosition(position);
+
+                // Show Alert
+
+                }
+
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.dash_board, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
