abdad49a0afef4cadb9e54bcbf954d95c2ff0e40
Jakob <Jakobhartman@hotmail.com>
2014-10-07 22:49:18 -0600

    selecting areas populates the houshold list views, last piece is to connect the house holds to a interview

--- app/src/main/java/com/example/chad/sra_mobile/DashBoard.java
+++ app/src/main/java/com/example/chad/sra_mobile/DashBoard.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 import LocalDatabase.Area;
 import LocalDatabase.DatabasePopulator;
+import LocalDatabase.Household;
 
 
 public class DashBoard extends Activity {
@@ -24,20 +25,23 @@ public class DashBoard extends Activity {
     Spinner spinner;
     ArrayList<String> areaValues;
     Area area;
+    Household household;
     List<Area> areas;
-    String[] values;
+    List<Household> households;
     ArrayAdapter<String> spinnerAdapter;
     ArrayAdapter<String> adapter;
+    DatabasePopulator create;
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        create = new DatabasePopulator();
         areaValues = new ArrayList<String>();
         area = new Area();
-        values = new String[] {
-                "Households"
-        };
+        household = new Household();
+        final ArrayList<String> householdValues = new ArrayList<String>();
+        householdValues.add("Households");
 
         areas = area.getAllAreas();
 
@@ -45,7 +49,7 @@ public class DashBoard extends Activity {
                 android.R.layout.simple_list_item_1, android.R.id.text1, areaValues);
 
         adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+                android.R.layout.simple_list_item_1, android.R.id.text1, householdValues);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
@@ -55,7 +59,6 @@ public class DashBoard extends Activity {
 
 
         areaValues.add("Select Area");
-
         for(int i = 0;i < areas.size();i++){
             String item = areas.get(i).name;
             areaValues.add(item);
@@ -93,7 +96,16 @@ public class DashBoard extends Activity {
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-
+                households = household.getHousehold(i);
+                householdValues.clear();
+                householdValues.add("Households");
+                for(int p = 0;p < households.size();++p){
+                    String item = households.get(p).name;
+                    householdValues.add(item);
+                }
+                System.out.println(householdValues);
+                adapter.notifyDataSetChanged();
+                System.out.println("Finished");
             }
 
             public void onNothingSelected(AdapterView<?> adapterView) {
@@ -124,11 +136,10 @@ public class DashBoard extends Activity {
     }
 
     public void syncDatabase(View v){
-
-        DatabasePopulator create = new DatabasePopulator();
         create.deleteAll();
         create.populate();
-
+        areaValues.clear();
+        areaValues.add("Select Area");
         for(int i = 0;i < areas.size();i++){
             String item = areas.get(i).name;
             areaValues.add(item);
