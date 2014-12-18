f43268af0b401b04c36f5345ce6f6afb34f9981c
Jakob Hartman <JakobHartman@git.com>
2014-12-18 13:39:44 -0700

    created singleton

--- app/src/main/java/com/sra/helperClasses/CRUDFlinger.java
+++ app/src/main/java/com/sra/helperClasses/CRUDFlinger.java
@@ -3,6 +3,10 @@ package com.sra.helperClasses;
 import android.app.Activity;
 import android.content.SharedPreferences;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.sra.objects.Region;
+
 import org.quickconnectfamily.json.JSONException;
 import org.quickconnectfamily.json.JSONUtilities;
 
@@ -14,8 +18,9 @@ import java.io.Serializable;
 public class CRUDFlinger {
 
     private static CRUDFlinger instance = null;
-    SharedPreferences loader;
-    SharedPreferences.Editor saver;
+    static SharedPreferences loader;
+    static SharedPreferences.Editor saver;
+    static Region region = null;
 
     protected CRUDFlinger(){
 
@@ -29,15 +34,51 @@ public class CRUDFlinger {
         return instance;
     }
 
-    public void setPreferences(Activity activity){
+    public static void setPreferences(Activity activity){
         loader = activity.getApplication().getSharedPreferences("AppPrefs", activity.getApplication().MODE_PRIVATE);
         saver = activity.getApplication().getSharedPreferences("AppPrefs", activity.getApplication().MODE_PRIVATE).edit();
     }
 
-    public void save(Serializable serializable, String key){
+    public static void save(String key,Serializable serializable){
         try{
             saver.putString(key,JSONUtilities.stringify(serializable));
             saver.commit();
         }catch (JSONException e){}
     }
+
+    public static Object load(String key,Class className){
+        String json = loader.getString(key,null);
+        Gson gson = new GsonBuilder().create();
+        Object object = gson.fromJson(json,className);
+
+        return object;
+    }
+
+    public static Region loadRegion(){
+        String json = loader.getString("Region",null);
+        Gson gson = new GsonBuilder().create();
+        Region region1 = gson.fromJson(json,Region.class);
+        return region1;
+    }
+
+    public static void saveRegion(){
+        if(region == null){
+            loadRegion();
+            return;
+        }else{
+            try{
+                saver.putString("Region",JSONUtilities.stringify(region));
+                saver.commit();
+            }catch (JSONException e){}
+        }
+   }
+
+    public static Region getRegion(){
+        if(region == null){
+            loadRegion();
+        }
+        return region;
+    }
+
+
 }
